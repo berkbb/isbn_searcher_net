9f39c2da1baa6bd02163fa3fcb01222ac97b7ccd
berkbb <berkbaba_dev@outlook.com>
2022-07-01 15:47:58 +0300

    Edit for Program.cs

--- ShowCase/Barcode.cs
+++ ShowCase/Barcode.cs
@@ -5,4 +5,6 @@ public class Barcode
     /// </summary>
     public string? barcode { get; set; }
 
+
 }
+
--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -6,14 +6,14 @@ using System.Text.Json;
 
 //Read the JSON file for gathering barcodes.
 string vscode_fileName = "barcodes.json";
-string vsmac_fileName = "/Users/berkbabadogan/Documents/GitHub/isbn_searcher_net/ShowCase/barcodes.json";
-string jsonString = File.ReadAllText(vsmac_fileName);
+//string vsmac_fileName = "/Users/berkbabadogan/Documents/GitHub/isbn_searcher_net/ShowCase/barcodes.json";
+string jsonString = File.ReadAllText(vscode_fileName);
 List<Barcode> barcodesList = JsonSerializer.Deserialize<List<Barcode>>(jsonString)!;
 
 //Print the gathered ISBN item with GetBookInfo() with given barcode .
 foreach (var item in barcodesList)
 {
-    var book_item = await Rest.GetBookInfoAsync(item.barcode);
+    var book_item = await Rest.GetBookInfoAsync(isbn: item.barcode!);
     ISBNElementExtensions.elements.Add(book_item);
     Console.WriteLine(book_item.ToString());
 }
